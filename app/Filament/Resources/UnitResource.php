782481e09cb14b590d4dadd88008e747f5a1a487
wabualela <wailbualela@gmail.com>
2023-02-10 23:45:06 +0200

    unit resource

---
+++ app/Filament/Resources/UnitResource.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UnitResource\Pages;
+use App\Filament\Resources\UnitResource\RelationManagers;
+use App\Models\Unit;
+use Filament\Forms;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UnitResource extends Resource
+{
+    protected static ?string $model = Unit::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    protected static ?string $label = 'الوحدة';
+
+    protected static ?string $pluralLabel = 'الوحدات';
+
+    protected static ?string $navigationGroup = 'المنتجات';
+
+    protected static ?int $navigationSort = 3;
+
+    protected static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make()
+                    ->schema([
+                        TextInput::make('name')
+                            ->label('اسم الوحدة')
+                            ->required()
+                            ->autofocus()
+                            ->autocomplete()
+                            ->alpha()
+                            ->minLength(3)
+                            ->maxLength(255)
+                    ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('اسم نوع المنتج')
+                    ->sortable()
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUnits::route('/'),
+            'create' => Pages\CreateUnit::route('/create'),
+            'edit' => Pages\EditUnit::route('/{record}/edit'),
+        ];
+    }
+}
