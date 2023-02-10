60945d6e4d1a5358b489064666526f305b27c061
wabualela <wailbualela@gmail.com>
2023-02-10 23:56:58 +0200

    add client

---
+++ database/factories/ClientFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Client>
+ */
+class ClientFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'tel' => $this->faker->phoneNumber(),
+            'address' => $this->faker->address()
+        ];
+    }
+}
