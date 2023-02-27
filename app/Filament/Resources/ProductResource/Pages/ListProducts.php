8d64eea993f07e128467a70d462b8922889731f1
Wail Abualela <wailabualela@gmail.com>
2023-02-27 17:05:02 +0200

    add filamentphp import & add it to product

--- app/Filament/Resources/ProductResource/Pages/ListProducts.php
+++ app/Filament/Resources/ProductResource/Pages/ListProducts.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\ProductResource\Pages;
 use App\Filament\Resources\ProductResource;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Konnco\FilamentImport\Actions\ImportAction;
+use Konnco\FilamentImport\Actions\ImportField;
 
 class ListProducts extends ListRecords
 {
@@ -14,6 +16,20 @@ class ListProducts extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+            ImportAction::make()
+                ->fields([
+                    ImportField::make('name')
+                        ->label('المنتج')
+                        ->required(),
+                    ImportField::make('selling_price')
+                        ->label('سعر البيع')
+                        ->required(),
+                    ImportField::make('purchasing_price')
+                        ->label('سعر الشراء')
+                        ->required(),
+                    ImportField::make('quantity')
+                        ->label('الكمية')
+                ])
         ];
     }
 }
