b9a877800df9f63849aab08c2c1cf7a00a6a8e68
Wail Abualela <wailabualela@gmail.com>
2023-04-29 18:39:05 +0100

    complete create steps

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -10,15 +10,20 @@ use App\Filament\Resources\OrderResource\Widgets\OrderStats;
 use App\Models\Order;
 use App\Models\Product;
 use Filament\Forms;
+use Filament\Forms\Components\Actions\Action;
 use Filament\Forms\Components\Card;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables;
+use Filament\Tables\Columns\BadgeColumn;
+use Filament\Tables\Columns\TextColumn;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
@@ -28,124 +33,133 @@ class OrderResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-shopping-bag';
 
-    protected static ?string $label = 'الطلب';
+    protected static ?string $label = 'طلب الشراء';
 
-    protected static ?string $pluralLabel = 'الطلبات';
+    protected static ?string $pluralLabel = 'طلبات الشراء';
 
     public static function form(Form $form): Form
     {
         return $form
-            ->schema(
-                [
+            ->schema([
                 Card::make()
                     ->schema(
                         [
+                            Grid::make(3)
+                                ->schema(
+                                    [
+                                        Select::make('client_id')
+                                            ->label('العميل')
+                                            ->createOptionForm(
+                                                [
+                                                    TextInput::make('name')
+                                                        ->label('اسم العميل')
+                                                        ->autocomplete('name')
+                                                        ->required(),
+                                                    TextInput::make('tel')
+                                                        ->label('رقم التلفون')
+                                                        ->autocomplete('tel')
+                                                        ->required(),
+                                                    TextInput::make('address')
+                                                        ->label('العنوان')
+                                                        ->autocomplete('address')
+                                                ]
+                                            )
+                                            ->searchable()
+                                            ->preload()
+                                            ->relationship('client', 'name'),
 
-                        Grid::make(3)
-                            ->schema(
-                                [
+                                        Select::make('product_id')
+                                            ->label('المنتج')
+                                            ->searchable()
+                                            ->preload()
+                                            // ->multiple()
+                                            ->relationship('product', 'name'),
 
-                                Select::make('client_id')
-                                    ->label('العميل')
-                                    ->createOptionForm(
-                                        [
-                                        TextInput::make('name')
-                                            ->label('اسم العميل')
-                                            ->autocomplete('name')
-                                            ->required(),
-                                        TextInput::make('tel')
-                                            ->label('رقم التلفون')
-                                            ->autocomplete('tel')
-                                            ->required(),
-                                        TextInput::make('address')
-                                            ->label('العنوان')
-                                            ->autocomplete('address')
-                                        ]
-                                    )
-                                    ->searchable()
-                                    ->preload()
-                                    ->relationship('client', 'name'),
+                                        TextInput::make('quantity')
+                                            ->label('الكمية')
+                                            ->default(0)
+                                            ->numeric(),
+                                    ]
+                                ),
 
-                                Select::make('product_id')
-                                    ->label('المنتج')
-                                    ->searchable()
-                                    ->preload()
-                                    // ->multiple()
-                                    ->relationship('product', 'name'),
+                            Grid::make()
+                                ->schema(
+                                    [
+                                        TextInput::make('total')
+                                            ->suffix('جنيه')
+                                            ->default(0)
+                                            ->numeric(),
+                                        TextInput::make('sub_total')
+                                            ->suffix('جنيه')
+                                            ->default(0)
+                                            ->numeric(),
+                                    ]
+                                ),
 
-                                TextInput::make('quantity')
-                                    ->label('الكمية')
-                                    ->default(0)
-                                    ->numeric(),
-                                ]
-                            ),
+                            Grid::make(3)
+                                ->schema(
+                                    [
+                                        TextInput::make('pay')
+                                            ->suffix('جنيه')
+                                            ->numeric(),
+                                        TextInput::make('due')
+                                            ->suffix('جنيه')
+                                            ->numeric(),
+                                        TextInput::make('vat')
+                                            ->suffix('%')
+                                            ->label('ضريبة')
+                                            ->numeric(),
+                                    ]
+                                ),
 
-                        Grid::make()
-                            ->schema(
-                                [
-                                TextInput::make('total')
-                                    ->suffix('جنيه')
-                                    ->default(0)
-                                    ->numeric(),
-                                TextInput::make('sub_total')
-                                    ->suffix('جنيه')
-                                    ->default(0)
-                                    ->numeric(),
-                                ]
-                            ),
-
-                        Grid::make(3)
-                            ->schema(
-                                [
-                                TextInput::make('pay')
-                                    ->suffix('جنيه')
-                                    ->numeric(),
-                                TextInput::make('due')
-                                    ->suffix('جنيه')
-                                    ->numeric(),
-                                TextInput::make('vat')
-                                    ->suffix('%')
-                                    ->label('ضريبة')
-                                    ->numeric(),
-                                ]
-                            ),
-
-                        Select::make('payment_method')
-                            ->label('الدفع عن طريق')
-                            ->options(
-                                [
-                                'كاش',
-                                'بنكك',
-                                ]
-                            ),
+                            Select::make('payment_method')
+                                ->label('الدفع عن طريق')
+                                ->options(
+                                    [
+                                        'كاش',
+                                        'بنكك',
+                                    ]
+                                ),
 
                         ]
                     )
-                ]
-            );
+            ]);
     }
 
     public static function table(Table $table): Table
     {
         return $table
-            ->columns(
-                [
-                //
-                ]
-            )
+            ->columns([
+                TextColumn::make('number')
+                    ->label(trans('Number')),
+                TextColumn::make('customer.name')
+                    ->label(trans('Customer Name')),
+                BadgeColumn::make('status')
+                    ->label(trans('Status'))
+                    ->colors([
+                        'danger' => 'cancelled',
+                        'warning' => 'processing',
+                        'success' => fn ($state) => in_array($state, ['delivered', 'shipped']),
+                    ]),
+                TextColumn::make('total_price')
+                    ->label(trans('Total Price')),
+                TextColumn::make('items_count')
+                    ->counts('items')
+                    ->label(trans('Product Count')),
+            ])
             ->filters(
                 [
-                //
+                    //
                 ]
             )
             ->actions(
                 [
-                Tables\Actions\EditAction::make(),
+                    Tables\Actions\EditAction::make(),
                 ]
             )
             ->bulkActions(
                 [
-                Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\DeleteBulkAction::make(),
                 ]
             );
     }
@@ -190,108 +204,85 @@ class OrderResource extends Resource
 
     public static function getFormSchema(?string $section = null): array
     {
-        if ($section === 'items') {
+        if ($section === 'items')
             return [
-                Forms\Components\Repeater::make('items')
+                Repeater::make('items')
+                    ->label(trans('Items'))
                     ->relationship()
-                    ->schema(
-                        [
-                        Forms\Components\Select::make('product_id')
-                            ->label('Product')
+                    ->schema([
+                        Select::make('product_id')
+                            ->label(trans('Product'))
                             ->options(Product::query()->pluck('name', 'id'))
                             ->required()
                             ->reactive()
-                            ->afterStateUpdated(fn ($state, callable $set) => $set('unit_price', Product::find($state)?->price ?? 0))
-                            ->columnSpan(
-                                [
-                                'md' => 5,
-                                ]
-                            ),
-
-                        Forms\Components\TextInput::make('qty')
+                            ->afterStateUpdated(fn ($state, callable $set) => $set('unit_price', Product::find($state)?->purchasing_price ?? 0))
+                            ->columnSpan(['md' => 5]),
+                        TextInput::make('qty')
+                            ->label(trans('gty'))
                             ->numeric()
                             ->default(1)
-                            ->columnSpan(
-                                [
-                                'md' => 2,
-                                ]
-                            )
+                            ->columnSpan(['md' => 2])
                             ->required(),
-
-                        Forms\Components\TextInput::make('unit_price')
-                            ->label('Unit Price')
+                        TextInput::make('unit_price')
+                            ->label(trans('Unit Price'))
                             ->disabled()
                             ->numeric()
                             ->required()
-                            ->columnSpan(
-                                [
-                                'md' => 3,
-                                ]
-                            ),
-                        ]
-                    )
+                            ->columnSpan(['md' => 3]),
+                    ])
                     ->orderable()
                     ->defaultItems(1)
                     ->disableLabel()
-                    ->columns(
-                        [
-                        'md' => 10,
-                        ]
-                    )
+                    ->columns(['md' => 10])
                     ->required(),
             ];
-        }
 
         return [
-            Forms\Components\TextInput::make('number')
+            TextInput::make('number')
+                ->label(trans('Order Number'))
                 ->default('OR-' . random_int(100000, 999999))
                 ->disabled()
                 ->required(),
-
-            Forms\Components\Select::make('customer_id')
+            Select::make('customer_id')
+                ->label(trans('Custmoer'))
                 ->relationship('customer', 'name')
                 ->searchable()
                 ->required()
-                ->createOptionForm(
-                    [
-                    Forms\Components\TextInput::make('name')
-                        ->label(__('app.customer_name'))
+                ->createOptionForm([
+                    TextInput::make('name')
+                        ->label(trans('app.customer_name'))
                         ->required(),
-                        Forms\Components\TextInput::make('email')
-                            ->label(__('app.email'))
-                            ->email()    
-                            ->required(),
-                    Forms\Components\TextInput::make('phone')
-                        ->label(__('app.phone')),
-                    Forms\Components\Select::make('gender')
-                            ->label(__('app.gender'))
-                            ->options(['male' => __('app.male'), 'female' => __('app.female')])
-                            ->searchable()
-                            ->required(),
-                    ]
-                )
+                    TextInput::make('email')
+                        ->label(trans('app.email'))
+                        ->email()
+                        ->required(),
+                    TextInput::make('phone')
+                        ->label(trans('app.phone')),
+                    Select::make('gender')
+                        ->label(trans('app.gender'))
+                        ->options(['male' => trans('app.male'), 'female' => trans('app.female')])
+                        ->searchable()
+                        ->required(),
+                ])
                 ->createOptionAction(
-                    function (Forms\Components\Actions\Action $action) {
+                    function (Action $action) {
                         return $action
-                            ->modalHeading('Create customer')
-                            ->modalButton('Create customer')
+                            ->modalHeading(trans('Add new customer'))
+                            ->modalButton(trans('Create'))
                             ->modalWidth('lg');
                     }
                 ),
 
-            Forms\Components\Select::make('status')
-                ->options(
-                    [
-                    'new' => 'New',
-                    'processing' => 'Processing',
-                    'shipped' => 'Shipped',
-                    'delivered' => 'Delivered',
-                    'cancelled' => 'Cancelled',
-                    ]
-                )
+            Select::make('status')
+                ->label(trans('Status'))
+                ->options([
+                    'new' => trans('New'),
+                    'processing' => trans('Processing'),
+                    'cancelled' => trans('Cancelled'),
+                ])
                 ->required(),
-
-            Forms\Components\MarkdownEditor::make('notes')
+            MarkdownEditor::make('notes')
+                ->label(trans('Note'))
                 ->columnSpan('full'),
         ];
     }
