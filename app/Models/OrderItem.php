ff7be6c4ef2bc266a3a0f6cf24601532b50d1ea7
Wail Abualela <wailabualela@gmail.com>
2023-04-29 18:39:22 +0100

    add fillable colms

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class OrderItem extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'product_id',
+        'order_id',
+        'qty',
+        'unit_price'
+    ];
 }
